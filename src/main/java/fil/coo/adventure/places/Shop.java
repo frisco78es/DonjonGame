36ca42a08c8e677fe762d5c21ff04313d2921e9d
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-01-10 10:36:47 +0100

    Clear Interact methods

--- src/main/java/fil/coo/adventure/places/Shop.java
+++ src/main/java/fil/coo/adventure/places/Shop.java
@@ -1,7 +1,9 @@
 package fil.coo.adventure.places;
 
+import fil.coo.adventure.entities.Player;
+
 public class Shop extends Room{
-    public void interact () {
+    public void interact (Player player) {
         // possibilité d'acheter des items en échange de gold
     }
     
