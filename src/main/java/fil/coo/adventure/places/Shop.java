5826289ba123d7c232e6f7f04fe792eaae1e43fb
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-01-10 12:21:04 +0100

    First version of the shop.

--- src/main/java/fil/coo/adventure/places/Shop.java
+++ src/main/java/fil/coo/adventure/places/Shop.java
@@ -1,23 +1,73 @@
 package fil.coo.adventure.places;
 
+import java.util.Scanner;
+
 import fil.coo.adventure.entities.Player;
 
 public class Shop extends Room {
-    public void interact (Player player) {
-        System.out.println("You found a not very welcoming shop offering you to buy some items in exchange of your precious gold.");
-        System.out.println("You can either enter the shop or leave. What would you do ?");
-        System.out.println("ENTER / LEAVE ?");
-        // TODO ask console to write ENTER or LEAVE
-        String playerAnswer = "ENTER";
-        switch(playerAnswer) {
-            case "ENTER":
-                System.out.println("You decide to enter the shop and are welcome by a small goblin, welcoming you to look at his items.");
-                System.out.println("You find some healing potions, being trade for 30 golds.");
-                System.out.println("You find some stength potions, being trade for 50 golds.");
-                System.out.println("What would you do ?");
-                System.out.println("HEAL / STRENGH / LEAVE ?");
-            case "LEAVE":
-                System.out.println("This shop don't look interesting, you are leaving this shop.");
-        }
-    }
+	public void interact(Player player) {
+		System.out.println(
+				"You found a not very welcoming shop offering you to buy some items in exchange of your precious gold.");
+		System.out.println("You can either enter the shop or leave. What would you do ?");
+		Boolean stay = true;
+		while (stay) {
+			System.out.println("ENTER / LEAVE ?");
+			Scanner sc = new Scanner(System.in);
+			String playerAnswer = sc.nextLine();
+			switch (playerAnswer.toLowerCase()) {
+				case "enter":
+					this.shop(player);
+					break;
+				case "leave":
+					System.out.println("This shop don't look interesting, you are leaving this shop.");
+					stay = false;
+					return;
+				default:
+					System.out.println("SYS: You entered a wrong command, please try again.");
+					break;
+			}
+		}
+	}
+
+	private void shop(Player player) {
+		System.out.println(
+				"You decide to enter the shop and are welcome by a small goblin, welcoming you to look at his items.");
+		System.out.println("You find some healing potions, being trade for 30 golds.");
+		System.out.println("You find some stength potions, being trade for 50 golds.");
+		System.out.println("What would you do ?");
+		Boolean stay = true;
+		while (stay) {
+			System.out.println("Current gold amount: " + player.getGold() + "g");
+			System.out.println("HEAL / STRENGTH / LEAVE ?");
+			Scanner sc = new Scanner(System.in);
+			String playerAnswer = sc.nextLine();
+			switch (playerAnswer.toLowerCase()) {
+				case "heal":
+					if (player.getGold() > 30) {
+						System.out.println("Thanks for buying a healing potion !");
+						player.removeGold(30);
+						player.RecoverLife(10);
+					} else {
+						System.out.println("You don't have enough gold for that");
+					}
+					break;
+				case "strength":
+					if (player.getGold() > 50) {
+						System.out.println("Thanks for buying a strength potion !");
+						player.removeGold(50);
+						player.addStrength(5);
+					} else {
+						System.out.println("You don't have enough gold for that");
+					}
+					break;
+				case "leave":
+					System.out.println("See you next time !");
+					stay = false;
+					return;
+				default:
+					System.out.println("SYS: You entered a wrong command, please try again.");
+					break;
+			}
+		}
+	}
 }
