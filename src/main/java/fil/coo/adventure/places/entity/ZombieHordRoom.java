612b184fe84634c9923f5a9558fa5e8a0f144061
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-01-11 11:31:26 +0100

    Correct Balance of the game

--- src/main/java/fil/coo/adventure/places/entity/ZombieHordRoom.java
+++ src/main/java/fil/coo/adventure/places/entity/ZombieHordRoom.java
@@ -1,6 +1,5 @@
 package fil.coo.adventure.places.entity;
 
-import fil.coo.adventure.entities.Player;
 import fil.coo.adventure.entities.monsters.util.ZombiesHord;
 import fil.coo.adventure.places.Room;
 
@@ -9,16 +8,5 @@ public class ZombieHordRoom extends Room {
         this.addMonster(new ZombiesHord());
         this.addMonster(new ZombiesHord());
         this.addMonster(new ZombiesHord());
-        this.addMonster(new ZombiesHord());
-        this.addMonster(new ZombiesHord());
-        this.addMonster(new ZombiesHord());
-    }
-
-    public void interact(Player player) {
-        System.out.println(
-                "This room is nasty, even if i've found something that look like a potion i'm not sure if i wan't to drink it.");
-        System.out.println("Drink it ?");
-        // TODO : allow the player to interact with this event
-        // Not drinking it end the event, drinking give him a strenght buff
     }
 }
