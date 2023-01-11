0842964b83419fbc5d364d018f6aabdee6696438
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-01-11 09:51:37 +0100

    Remove unused imports

--- src/main/java/fil/coo/adventure/places/GoblinRoom.java
+++ src/main/java/fil/coo/adventure/places/GoblinRoom.java
@@ -1,6 +1,5 @@
 package fil.coo.adventure.places;
 
-import fil.coo.adventure.entities.items.util.LifePotion;
 import fil.coo.adventure.entities.monsters.util.Goblin;
 
 public class GoblinRoom extends Room {
