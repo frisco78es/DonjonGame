0a21dc5dddda42f054fddd889edc5f7f6a83324b
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-01-11 09:02:57 +0100

    Delete unused import

--- src/main/java/fil/coo/adventure/Main.java
+++ src/main/java/fil/coo/adventure/Main.java
@@ -1,14 +1,12 @@
 package fil.coo.adventure;
 
-import fil.coo.adventure.AdventureGame;
 import fil.coo.adventure.places.Room;
 import fil.coo.adventure.places.Donjon;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		AdventureGame aG = new AdventureGame(); // Méthode generateMap à compléter
+		AdventureGame aG = new AdventureGame();
 		Donjon map = new Donjon(5);
 		Room startingRoom = map.generateMap();
 		aG.setCurrentRoom(startingRoom);
