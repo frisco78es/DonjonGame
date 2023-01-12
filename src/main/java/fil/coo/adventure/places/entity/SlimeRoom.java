be969f057c77b478da5b31460ff615151220cb31
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-01-12 17:17:16 +0100

    Correct slime room bug

--- src/main/java/fil/coo/adventure/places/entity/SlimeRoom.java
+++ src/main/java/fil/coo/adventure/places/entity/SlimeRoom.java
@@ -28,19 +28,24 @@ public class SlimeRoom extends Room {
                 "This room is nasty, even if i've found something that look like a potion i'm not sure if i wan't to drink it.");
         System.out.println("Drink it ?");
         System.out.println("YES / NO ?");
-        String playerAnswer = this.scanner.nextLine();
-        switch (playerAnswer.toLowerCase()) {
-            case "yes":
-                player.addStrength(5);
-                System.out.println("This potion have a nasty taste, buy you seem more powerful after drinking it.");
-                System.out.println("SYS: 5 Strength added to your character.");
-                break;
-            case "no":
-                System.out.println("Too dangerous for you, you're leaving.");
-                break;
-            default:
-                System.out.println("SYS: You entered a wrong command, please try again.");
-                break;
+        Boolean bool = true;
+        while (bool) {
+            String playerAnswer = this.scanner.nextLine();
+            switch (playerAnswer.toLowerCase()) {
+                case "yes":
+                    player.addStrength(5);
+                    System.out.println("This potion have a nasty taste, buy you seem more powerful after drinking it.");
+                    System.out.println("SYS: 5 Strength added to your character.");
+                    bool = false;
+                    break;
+                case "no":
+                    System.out.println("Too dangerous for you, you're leaving.");
+                    bool = false;
+                    break;
+                default:
+                    System.out.println("SYS: You entered a wrong command, please try again.");
+                    break;
+            }
         }
     }
 }
