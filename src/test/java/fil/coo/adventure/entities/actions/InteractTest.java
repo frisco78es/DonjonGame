8fc3e1f596d0048ce3f10ed047eaa3e577d5823a
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-01-13 14:44:49 +0100

    Correcting Interact test

--- src/test/java/fil/coo/adventure/entities/actions/InteractTest.java
+++ src/test/java/fil/coo/adventure/entities/actions/InteractTest.java
@@ -5,33 +5,33 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import fil.coo.adventure.entities.Player;
-import fil.coo.adventure.entities.actions.util.Use;
+import fil.coo.adventure.entities.actions.util.Interact;
 import fil.coo.adventure.places.Room;
 import fil.coo.adventure.places.entity.TrappedRoom;
 
-public class UseTest {
+public class InteractTest {
 
     @Test
     public void isPossibleToUseUse() {
         Room r = new Room();
-        Use u = new Use();
+        Interact i = new Interact();
 
-        assertEquals("should is posible to use room", true, u.isPossible(r));
+        assertEquals("should is posible to use room", true, i.isPossible(r));
 
         r.interact(new Player());
 
-        assertEquals("should is not possible to second use Use", false, u.isPossible(r));
+        assertEquals("should is not possible to second use Use", false, i.isPossible(r));
     }
 
     @Test
     public void useUse() {
         Room r = new TrappedRoom();
         Player p = new Player();
-        Use u = new Use();
+        Interact i = new Interact();
 
         assertEquals("should be nombre of monstre is zero", 0, r.getMonsters().size());
 
-        u.execute(r, p);
+        i.execute(r, p);
 
         assertEquals("should use use in the room and monsters is equal 2", 2, r.getMonsters().size());
     }
